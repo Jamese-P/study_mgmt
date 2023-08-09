1d2e46da8a8371644b42c8085cd57f2161d4b851
Jamese-P <syuntaokuhara0812@gmail.com>
2023-08-09 11:58:00 +0900

    today modify

--- resources/views/today.blade.php
+++ resources/views/today.blade.php
@@ -48,84 +48,88 @@
                     @endfor
                 </table>
             </div>
-            
         @endforeach
         
     <h1>Tommorow {{\Carbon\Carbon::tomorrow()->format('Y/m/d')}}</h1>
-        <div class='book'>
-        @foreach($books_today as $book)
-            @if ($book->intarval_id=='1')
-                <h2><a href="/books/{{$book->id}}">{{$book->name}}</a></h2>
+        @foreach($books_today as $book_mgmt)
+            @if ($book_mgmt->intarval_id=='1' && $book_mtmg->book->max > $book_mtmg->finished+$book_mgmt->a_day)
+            <div class='book'>
+                <h2><a href="/books/{{$book_mtmg->book_id}}">{{$book_mtmg->book->name}}</a></h2>
                 <table>
-                    @for ($i = 0; $i<($book->a_day); $i++)
-                        @if ($book->max >= $book->finished+$book->a_day+$i+1)
+                    @for ($i = 0; $i<($book_mtmg->a_day); $i++)
+                    @if ($book_mtmg->book->max >= $book_mtmg->finished+$book_mgmt->a_day+$i+1)
                         <tr>
-                            @if ($book->today_finished == ($book->finished +$book->a_day+ $i))
+                            @if ($book_mtmg->today_finished == ($book_mtmg->finished+$book_mtmg->a_day + $i))
                             <th>
-                                <a href="/today/{{$book->id}}/{{$book->finished +$book->a_day+ $i+1}}/complete">{{$book->type->name}}{{$book->finished+$book->a_day+$i+1}}</a>
+                                <a href="/today/{{$book_mtmg->book_id}}/{{$book_mtmg->finished+$book_mgmt->a_day + $i+1}}/complete">
+                                    {{$book_mtmg->book->type->name}}{{$book_mtmg->finished+$book_mgmt->a_day+$i+1}}
+                                </a>
                             </th>
                             @else
                             <th>
-                                {{$book->type->name}}{{$book->finished+$book->a_day+$i+1}}
+                                {{$book_mtmg->book->type->name}}{{$book_mtmg->finished+$book_mgmt->a_day+$i+1}}
                             </th>
                             @endif
-                            @if ($book->today_finished == ($book->finished +$book->a_day+ $i))
-                            <th>
-                                <form action="/today/{{$book->id}}/complete" id="form_{{$book->id}}_complete" method="post">
-                                    @csrf
-                                    <button type="button" onclick="complete({{$book->id}})">complete</button>
-                                </form>
-                            </th>
-                            <th>
-                                <form action="/today/{{$book->id}}/pass" id="form_{{$book->id}}_pass" method="post">
-                                    @csrf
-                                    <button type="button" onclick="pass({{$book->id}})">pass</button>
-                                </form>
-                            </th>
+                            @if ($book_mtmg->today_finished == ($book_mtmg->finished+$book_mgmt->a_day + $i))
+                                <th>
+                                    <form action="/today/{{$book_mtmg->book_id}}/complete" id="form_{{$book_mtmg->book_id}}_complete" method="post">
+                                        @csrf
+                                        <button type="button" onclick="complete({{$book_mtmg->book_id}})">complete</button>
+                                    </form>
+                                </th>
+                                <th>
+                                    <form action="/today/{{$book_mtmg->book_id}}/pass" id="form_{{$book_mtmg->book_id}}_pass" method="post">
+                                        @csrf
+                                        <button type="button" onclick="pass({{$book_mtmg->book_id}})">pass</button>
+                                    </form>
+                                </th>
                             @endif
                         </tr>
-                        @endif
+                    @endif
                     @endfor
                 </table>
+            </div>
             @endif
         @endforeach
         
-        @foreach($books_tomorrow as $book)
-            
-            <h2><a href="/books/{{$book->id}}">{{$book->name}}</a></h2>
-            <table>
-                @for ($i = 0; $i<($book->a_day); $i++)
-                @if ($book->max >= $book->finished+$i+1)
-                    <tr>
-                        @if ($book->today_finished == ($book->finished + $i))
-                        <th>
-                            <a href="/today/{{$book->id}}/{{$book->finished + $i+1}}/complete">{{$book->type->name}}{{$book->finished+$i+1}}</a>
-                        </th>
-                        @else
-                        <th>
-                            {{$book->type->name}}{{$book->finished+$i+1}}
-                        </th>
-                        @endif
-                        @if ($book->today_finished == ($book->finished + $i))
-                        <th>
-                            <form action="/today/{{$book->id}}/complete" id="form_{{$book->id}}_complete" method="post">
-                                @csrf
-                                <button type="button" onclick="complete({{$book->id}})">complete</button>
-                            </form>
-                        </th>
-                        <th>
-                            <form action="/today/{{$book->id}}/pass" id="form_{{$book->id}}_pass" method="post">
-                                @csrf
-                                <button type="button" onclick="pass({{$book->id}})">pass</button>
-                            </form>
-                        </th>
-                        @endif
-                    </tr>
-                @endif
-                @endfor
-            </table>
+        @foreach($books_tomorrow as $book_mtmg)
+            <div class='book'>
+                <h2><a href="/books/{{$book_mtmg->book_id}}">{{$book_mtmg->book->name}}</a></h2>
+                <table>
+                    @for ($i = 0; $i<($book_mtmg->a_day); $i++)
+                    @if ($book_mtmg->book->max >= $book_mtmg->finished+$i+1)
+                        <tr>
+                            @if ($book_mtmg->today_finished == ($book_mtmg->finished + $i))
+                            <th>
+                                <a href="/today/{{$book_mtmg->book->id}}/{{$book_mtmg->finished + $i+1}}/complete">
+                                    {{$book_mtmg->book->type->name}}{{$book_mtmg->finished+$i+1}}
+                                </a>
+                            </th>
+                            @else
+                            <th>
+                                {{$book_mtmg->book->type->name}}{{$book_mtmg->finished+$i+1}}
+                            </th>
+                            @endif
+                            @if ($book_mtmg->today_finished == ($book_mtmg->finished + $i))
+                                <th>
+                                    <form action="/today/{{$book_mtmg->book_id}}/complete" id="form_{{$book_mtmg->book_id}}_complete" method="post">
+                                        @csrf
+                                        <button type="button" onclick="complete({{$book_mtmg->book_id}})">complete</button>
+                                    </form>
+                                </th>
+                                <th>
+                                    <form action="/today/{{$book_mtmg->book_id}}/pass" id="form_{{$book_mtmg->book_id}}_pass" method="post">
+                                        @csrf
+                                        <button type="button" onclick="pass({{$book_mtmg->book_id}})">pass</button>
+                                    </form>
+                                </th>
+                            @endif
+                        </tr>
+                    @endif
+                    @endfor
+                </table>
+            </div>
         @endforeach
-        </div>
 
     <a href="/">参考書一覧</a>
     <br>

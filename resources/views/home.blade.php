32664112ceb661e292cedd547b61e7cb54183052
Jamese-P <syuntaokuhara0812@gmail.com>
2023-09-04 16:50:00 +0900

    feat home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,22 @@
         <title>Login</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/today') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Today</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                    @endif
-                @endauth
+            <div class="flex justify-center">
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <h1 class="txt-h1"><a href="{{ url('/today') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Today</a></h1>
+                    @else
+                        <h1><a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></h1>
+    
+                        @if (Route::has('register'))
+                            <h1><a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></h1>
+                        @endif
+                    @endauth
+                </div>
             </div>
         @endif
     </body>

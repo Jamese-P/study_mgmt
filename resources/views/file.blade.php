32fc7db335624eb4ec299db54079034e0c36e670
Jamese-P <syuntaokuhara0812@gmail.com>
2023-10-27 10:37:09 +0900

    files

--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -10,8 +10,10 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
     <body>
-        <a href="{{route('file1')}}">最重要文法語法</a>
-        <a href="{{route('file2')}}">中学英文整序</a>
+        
+        @foreach($files as $file)
+        <a href="files/{{$file->getfileName()}}"><p>{{$file->getfileName()}}</p></a>
+        @endforeach
         
     </body>
 </html>

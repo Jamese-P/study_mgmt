2cb293e62e5a33617967a31401079688393dce9d
Jamese-P <syuntaokuhara0812@gmail.com>
2024-02-09 13:11:24 +0900

    file details

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,47 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use File;
 use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('file.index');
     }
 
-    public function show_base(){
-        $path=public_path('files/');
-        $files = \File::files($path);
+    public function show_base()
+    {
+        $path = public_path('files/');
+        $files = File::files($path);
         natsort($files);
         $path = 'files/';
-        return view('file.file')->with(['path'=>$path,'files'=>$files]);
+
+        return view('file.file')->with(['path' => $path, 'files' => $files]);
     }
 
-    public function show_high(){
-        $path=public_path('high/');
-        $files = \File::files($path);
+    public function show_high()
+    {
+        $path = public_path('high/');
+        $files = File::files($path);
         natsort($files);
         $path = 'high/';
         $text = Storage::get('url.txt');
-        $urls = explode("\n",$text);
-        return view('file.file')->with(['urls'=>$urls,'path'=>$path,'files'=>$files]);
+        $urls = explode("\n", $text);
+
+        return view('file.file')->with(['urls' => $urls, 'path' => $path, 'files' => $files]);
     }
 
-    public function sinken(){
-        $path=public_path('sinken/');
-        $files = \File::files($path);
-        natsort($files);
+    public function sinken()
+    {
         $path = 'sinken/';
-        return view('file.file')->with(['path'=>$path,'files'=>$files]);
+        return view('file.sinken')->with(['path' => $path]);
     }
 
-    public function eiken(){
-        $path=public_path('eiken/');
-        $files = \File::files($path);
-        natsort($files);
+    public function eiken()
+    {
         $path = 'eiken/';
-        return view('file.file')->with(['path'=>$path,'files'=>$files]);
+        return view('file.eiken')->with(['path' => $path]);
     }
 }

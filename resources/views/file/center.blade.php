c2beae72152e3b930eda9ef62065c493e2fe4ad8
Shunta Okuhara <syuntaokuhara0812@gmail.com>
2024-02-21 11:13:45 +0900

    format

--- resources/views/file/center.blade.php
+++ resources/views/file/center.blade.php
@@ -5,41 +5,41 @@
             <summary>国語</summary>
             <ul>
                 @php
-                    $path_tmp = $path."japanese/";
+                    $path_tmp = $path . 'japanese/';
                     $path_pre = public_path($path_tmp);
                     $path_pre_count = mb_strlen($path_pre);
                     $files = File::files(public_path($path_tmp));
                 @endphp
                 @foreach ($files as $file)
-                        @php
-                            $file_name = substr($file, $path_pre_count);
-                        @endphp
-                         <li>
-                            <a href="/{{ $path_tmp }}{{ $file_name }}" target="_blank">
-                                {{ $file_name }}
-                            </a>
-                        </li>
+                    @php
+                        $file_name = substr($file, $path_pre_count);
+                    @endphp
+                    <li>
+                        <a href="/{{ $path_tmp }}{{ $file_name }}" target="_blank">
+                            {{ $file_name }}
+                        </a>
+                    </li>
                 @endforeach
             </ul>
         </details>
         <details>
             <summary>英語</summary>
             <ul>
-@php
-                    $path_tmp = $path."english/";
+                @php
+                    $path_tmp = $path . 'english/';
                     $path_pre = public_path($path_tmp);
                     $path_pre_count = mb_strlen($path_pre);
                     $files = File::files(public_path($path_tmp));
                 @endphp
                 @foreach ($files as $file)
-                        @php
-                            $file_name = substr($file, $path_pre_count);
-                        @endphp
-                         <li>
-                            <a href="/{{ $path_tmp }}{{ $file_name }}" target="_blank">
-                                {{ $file_name }}
-                            </a>
-                        </li>
+                    @php
+                        $file_name = substr($file, $path_pre_count);
+                    @endphp
+                    <li>
+                        <a href="/{{ $path_tmp }}{{ $file_name }}" target="_blank">
+                            {{ $file_name }}
+                        </a>
+                    </li>
                 @endforeach
             </ul>
         </details>
